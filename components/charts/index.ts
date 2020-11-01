5dab6224e0b494cd2b7b64624883427debc23bf6
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-11-01 14:32:23 +0700

    ✨ Wrapper file for importing chart components

---
+++ components/charts/index.ts
@@ -0,0 +1,5 @@
+import { BarChartVisualization as BarChart } from "./BarChart";
+import { LineChartVisualization as LineChart } from "./LineChart";
+import { PieChartVisualization as PieChart } from "./PieChart";
+
+export {BarChart, LineChart, PieChart}
