118476b641a04f7d524f70a24fe7f867a5273a4f
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-11-03 21:15:02 +0700

    ♻️ Simplify getMedian function

--- utils/getMedian.ts
+++ utils/getMedian.ts
@@ -1,17 +1,10 @@
 function getMedian(array: number[]): number {
   const arrayLength = array.length
-  const isArrayLengthEven = arrayLength % 2 === 0 ? true : false
+  let firstMidIndex = arrayLength / 2
+  let secondMidIndex = firstMidIndex + 1
+  let result = (array[firstMidIndex] + array[secondMidIndex]) / 2
 
-  if (isArrayLengthEven) {
-    let firstMidIndex = arrayLength / 2
-    let secondMidIndex = firstMidIndex + 1
-    let result = (array[firstMidIndex] + array[secondMidIndex]) / 2
-
-    return result
-  } else {
-    let medianIndex = (arrayLength + 1) / 2
-    return array[medianIndex]
-  }
+  return result
 }
 
 export default getMedian
