7676681e972c7d9e4dc544fc2a33831bea3ac34f
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-11-03 20:31:48 +0700

    ♻️ Make PieChart component reusable

--- components/charts/PieChart.tsx
+++ components/charts/PieChart.tsx
@@ -1,14 +1,35 @@
 import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from "recharts"
-import { NormalFreqDistData } from "../freq-dist-tables/Normal"
 
-const PieChartVisualization = () => {
+interface PropsType {
+  data: any[]
+  dataKeys: [string, string]
+  nameKey: string
+  legendLabels: [string, string]
+}
+
+const PieChartVisualization: React.FC<PropsType> = ({
+  data,
+  dataKeys,
+  nameKey,
+  legendLabels,
+}) => {
+  const legendPayload = [
+    {
+      value: legendLabels[0],
+      type: "circle",
+      id: "ID01",
+      color: "#5D737E",
+    },
+    { value: legendLabels[1], type: "circle", id: "ID02", color: "#000" },
+  ]
+
   return (
     <ResponsiveContainer width="100%" height={350}>
       <PieChart>
         <Pie
-          data={NormalFreqDistData}
-          dataKey="frekuensi"
-          nameKey="interval"
+          data={data}
+          dataKey={dataKeys[0]}
+          nameKey={nameKey}
           cx="50%"
           cy="50%"
           outerRadius={40}
@@ -16,9 +37,9 @@ const PieChartVisualization = () => {
           fill="#5D737E"
         />
         <Pie
-          data={NormalFreqDistData}
-          dataKey="persentase"
-          nameKey="interval"
+          data={data}
+          dataKey={dataKeys[1]}
+          nameKey={nameKey}
           cx="50%"
           cy="50%"
           innerRadius={90}
@@ -26,17 +47,7 @@ const PieChartVisualization = () => {
           label
           fill="#000"
         />
-        <Legend
-          payload={[
-            {
-              value: "Frekuensi",
-              type: "circle",
-              id: "ID01",
-              color: "#5D737E",
-            },
-            { value: "Persentase", type: "circle", id: "ID02", color: "#000" },
-          ]}
-        />
+        <Legend payload={legendPayload} />
         <Tooltip />
       </PieChart>
     </ResponsiveContainer>
