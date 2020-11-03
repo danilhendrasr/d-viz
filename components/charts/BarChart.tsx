0bfda343103fe6c742ee527f9020973f9d23a66f
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-11-03 14:29:41 +0700

    ♻️ Turn BarChart into a reusable component

--- components/charts/BarChart.tsx
+++ components/charts/BarChart.tsx
@@ -8,24 +8,46 @@ import {
   Bar,
   ResponsiveContainer,
 } from "recharts"
-import { NormalFreqDistData } from "../freq-dist-tables/Normal"
 
-const BarChartVisualization = () => {
+interface LegendPayload {
+  value: string
+  type: string
+  id: string
+  color: string
+}
+
+const BarChartVisualization: React.FC<{
+  data: object[]
+  xAxisKeyName: string
+  barDataKeys: [string, string]
+  labels: [string, string]
+}> = ({ data, xAxisKeyName, barDataKeys, labels }) => {
+  const colors: [string, string] = ["#5D737E", "#000000"]
+  const payload: LegendPayload[] = [
+    {
+      value: labels[0],
+      type: "rect",
+      id: "id01",
+      color: colors[0],
+    },
+    {
+      value: labels[1],
+      type: "rect",
+      id: "id02",
+      color: colors[1],
+    },
+  ]
+
   return (
     <ResponsiveContainer width="100%" height={350}>
-      <BarChart data={NormalFreqDistData}>
+      <BarChart data={data}>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="interval" interval={0} />
+        <XAxis dataKey={xAxisKeyName} interval={0} />
         <YAxis />
         <Tooltip />
-        <Legend
-          payload={[
-            { value: "Persentase", type: "rect", id: "ID01", color: "#5D737E" },
-            { value: "Frekuensi", type: "rect", id: "ID01", color: "#000" },
-          ]}
-        />
-        <Bar dataKey="frekuensi" fill="#5D737E" />
-        <Bar dataKey="persentase" fill="#000000" />
+        <Legend payload={payload} />
+        <Bar dataKey={barDataKeys[0]} fill={colors[0]} name={labels[0]} />
+        <Bar dataKey={barDataKeys[1]} fill={colors[1]} name={labels[1]} />
       </BarChart>
     </ResponsiveContainer>
   )
