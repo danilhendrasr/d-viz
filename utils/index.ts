162e9e4fbe52fddaa2bcddba2bda8fcfee6a6ba8
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-11-03 21:14:32 +0700

    🔥 Delete unused utility functions

--- utils/index.ts
+++ utils/index.ts
@@ -1,19 +1,6 @@
 import { default as getMean } from "./getMean"
 import { default as getMode } from "./getMode"
 import { default as getMedian } from "./getMedian"
-import {
-  getData,
-  extractValuesFromRecords,
-  getFrequencyTableFromRecordValues,
-  getFreqDistObject,
-} from "./mainUtils"
+import { getData, extractValuesFromRecords } from "./mainUtils"
 
-export {
-  getMean,
-  getMode,
-  getMedian,
-  getData,
-  extractValuesFromRecords,
-  getFrequencyTableFromRecordValues,
-  getFreqDistObject,
-}
+export { getMean, getMode, getMedian, getData, extractValuesFromRecords }
