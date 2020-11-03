3aa0e6d9bcfdceacc80f73e0342e729c3f043cd3
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-11-03 20:11:16 +0700

    ♻️ Simplify BarChart component

--- components/charts/BarChart.tsx
+++ components/charts/BarChart.tsx
@@ -9,35 +9,21 @@ import {
   ResponsiveContainer,
 } from "recharts"
 
-interface LegendPayload {
-  value: string
-  type: string
-  id: string
-  color: string
-}
-
-const BarChartVisualization: React.FC<{
+interface PropsType {
   data: object[]
   xAxisKeyName: string
   barDataKeys: [string, string]
   labels: [string, string]
-}> = ({ data, xAxisKeyName, barDataKeys, labels }) => {
-  const colors: [string, string] = ["#5D737E", "#000000"]
-  const payload: LegendPayload[] = [
-    {
-      value: labels[0],
-      type: "rect",
-      id: "id01",
-      color: colors[0],
-    },
-    {
-      value: labels[1],
-      type: "rect",
-      id: "id02",
-      color: colors[1],
-    },
-  ]
+}
 
+const colors: [string, string] = ["#5D737E", "#000000"]
+
+const BarChartVisualization: React.FC<PropsType> = ({
+  data,
+  xAxisKeyName,
+  barDataKeys,
+  labels,
+}) => {
   return (
     <ResponsiveContainer width="100%" height={350}>
       <BarChart data={data}>
@@ -45,7 +31,7 @@ const BarChartVisualization: React.FC<{
         <XAxis dataKey={xAxisKeyName} interval={0} />
         <YAxis />
         <Tooltip />
-        <Legend payload={payload} />
+        <Legend />
         <Bar dataKey={barDataKeys[0]} fill={colors[0]} name={labels[0]} />
         <Bar dataKey={barDataKeys[1]} fill={colors[1]} name={labels[1]} />
       </BarChart>
