3be64c44abcfbd181ce39068993179ba7172e1ce
Danil Hendra <danilhendrasuryawan@gmail.com>
2020-11-04 14:16:11 +0700

    ♻️ Refactor util functions export structure

--- utils/index.ts
+++ utils/index.ts
@@ -2,5 +2,16 @@ import { default as getMean } from "./getMean"
 import { default as getMode } from "./getMode"
 import { default as getMedian } from "./getMedian"
 import { getData, extractValuesFromRecords } from "./mainUtils"
+import { getQuartiles } from "./quartiles"
+import { getVariants, getStdDeviation } from "./others"
 
-export { getMean, getMode, getMedian, getData, extractValuesFromRecords }
+export {
+  getMean,
+  getMode,
+  getMedian,
+  getData,
+  extractValuesFromRecords,
+  getQuartiles,
+  getVariants,
+  getStdDeviation,
+}
